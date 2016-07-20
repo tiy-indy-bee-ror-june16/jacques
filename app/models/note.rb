fee75d9554c9fc16f3bcc59eff408ec821614052
Marie Denney <mldenne@gmail.com>
2016-07-20 14:39:14 -0400

    Corrected Error #1 Tag should require name to be set

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,8 @@
 class Note < ApplicationRecord
 
-  belongs_to :author
+  belongs_to :user
+  validates :title, presence: true
+  validates :body, presence: true
 
   has_many :taggings
   has_many :tags, through: :taggings
