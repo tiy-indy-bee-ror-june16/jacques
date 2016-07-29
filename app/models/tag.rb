1df5482a3306df8f25ff77506f08e90a980e2f16
rittersm <rittersm@gmail.com>
2016-07-29 14:07:04 -0400

    added uniqueness to tag validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,6 +4,6 @@ class Tag < ApplicationRecord
   has_many :taggings
   has_many :notes, through: :taggings, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
 end
