a309e9341eea4ba1caf72307c9d102e0dc90c7d7
rittersm <rittersm@gmail.com>
2016-07-29 13:45:56 -0400

    dependent destroy added to note/tag

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,7 @@
 class Note < ApplicationRecord
 
   has_many :taggings
-  has_many :tags, through: :taggings
+  has_many :tags, through: :taggings, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ApplicationRecord
   default_scope { order(created_at: :asc) }
 
   has_many :taggings
-  has_many :notes, through: :taggings
+  has_many :notes, through: :taggings, dependent: :destroy
 
   validates :name, presence: true
 
