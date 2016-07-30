5bd3941bfbd2a96063bf13abe6f955216a0eb47a
rittersm <rittersm@gmail.com>
2016-07-30 17:48:35 -0400

    once more

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
 
   scope :api do
     resources :users
-    resources :tags, except: [:show]
+    resources :tags
     resources :notes
 
   end
-
-  get 'tag/:name(.:format)' => 'tags#show'
 
 end
