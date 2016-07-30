7751907db437cdfb0b755bba09b8fab468121a7e
rittersm <rittersm@gmail.com>
2016-07-30 17:46:57 -0400

    trying anything

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     resources :users
     resources :tags, except: [:show]
     resources :notes
-    get 'tag/:name(.:format)' => 'tags#show'
+
   end
+
+  get 'tag/:name(.:format)' => 'tags#show'
 
 end
