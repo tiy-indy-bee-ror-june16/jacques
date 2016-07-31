4d2b18f7157b3e0aebe54f8e92e5e045ae1c91ce
rittersm <rittersm@gmail.com>
2016-07-31 12:25:56 -0400

    tag serializer and show method

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -10,8 +10,6 @@ class TagsController < ApplicationController
 
   # GET /tags/1
   def show
-    @tag = Tag.find_by(name: params[:name])
-
     render json: @tag, serializer: TagSerializer
   end
 
--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,5 +1,4 @@
 class TagSerializer < ActiveModel::Serializer
-  attributes :name
 
   has_many :notes
 
